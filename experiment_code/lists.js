1b444ab15079de5f5acd38cd01f2459d2cd41616
Abhilasha Kumar <abhilashak9@gmail.com>
2022-10-31 09:56:16 -0400

    sample experiment

---
+++ experiment_code/lists.js
@@ -0,0 +1,13 @@
+
+var lists = [
+  {
+    "ListNo": "51",
+    "items": ["cat", "dog", "mouse", "rat", "giraffe", "lion","zebra", "rhino", "ostrich"]
+
+  },
+  {
+    "ListNo": "4234",
+    "items": ["donkey", "dog", "fish"]
+
+  }
+]
